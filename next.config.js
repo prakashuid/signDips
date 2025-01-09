de8885009c18c8c67a5d1d82641177fd9fb706a9
PR51 <pr51@ford.com>
2025-01-09 22:50:57 +0530

    next config changes
    next config changes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
+// /** @type {import('next').NextConfig} */
+// const withPWA = require('next-pwa')({
+//   dest: 'public',
+//   // disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
+// });
+
+// const nextConfig = {
+// };
+
+// module.exports = withPWA(nextConfig);
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-});
-
 const nextConfig = {
-};
-
-module.exports = withPWA(nextConfig);
+  /* config options here */
+}
+ 
+module.exports = nextConfig
