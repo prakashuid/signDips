e07bb512e66f86345b97e277674ed8f19d4c82ab
PR51 <pr51@ford.com>
2025-01-09 20:59:58 +0530

    update commit
    update commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
+  disable: process.env.NODE_ENV === 'development',
 });
 
 const nextConfig = {
